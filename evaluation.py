d60434865a337836243a6b07024518883ed3dddb
Srijay Deshpande <deshpandesrijay@gmail.com>
2024-03-11 08:15:07 +0000

    Add file

---
+++ evaluation.py
@@ -0,0 +1,47 @@
+"""
+
+Script to construct boxplots, computation of mean, standard deviation and p-values
+
+"""
+
+import matplotlib.pyplot as plt
+import json
+import numpy as np
+from scipy.stats import mannwhitneyu
+
+spaderesnet_dice_path = r'F:\Datasets\BCSS_InstaDeep\splits\test\cropped\768\results_kaggle\spaderesnet\spaderesnet_auc.json'
+unet_dice_path = r'F:\Datasets\BCSS_InstaDeep\splits\test\cropped\768\results\unet\unet_auc.json'
+unetpp_dice_path = r'F:\Datasets\BCSS_InstaDeep\splits\test\cropped\768\results\unet++\unet++_auc.json'
+
+metric = 'AUC-ROC'
+
+tissue_id_dict = {1: 'Tumor', 2: 'Stroma', 3: 'Inflammatory', 4: 'Necrosis', 5: 'Other'}
+
+with open(spaderesnet_dice_path, 'r') as json_file:
+    spaderesnet_loaded_dict = json.load(json_file)
+
+with open(unet_dice_path, 'r') as json_file:
+    unet_loaded_dict = json.load(json_file)
+
+with open(unetpp_dice_path, 'r') as json_file:
+    unetpp_loaded_dict = json.load(json_file)
+
+labels = ['SPADE-ResNet', 'UNet', 'UNet++']
+colors = ['lightblue', 'lightcoral', 'lightgreen']
+
+for label in range(1, 6):
+    print('Mean and Std for SPADESegResNet, ',tissue_id_dict[label], ' is ',np.mean(spaderesnet_loaded_dict[str(label)]), ' std: ', np.std(spaderesnet_loaded_dict[str(label)]))
+    print('Mean and Std for UNet, ',tissue_id_dict[label], ' is ',np.mean(unet_loaded_dict[str(label)]), ' std: ', np.std(unet_loaded_dict[str(label)]))
+    print('Mean and Std for UNet++, ',tissue_id_dict[label], ' is ',np.mean(unetpp_loaded_dict[str(label)]), ' std: ', np.std(unetpp_loaded_dict[str(label)]))
+    U1, p_unet = mannwhitneyu(spaderesnet_loaded_dict[str(label)], unet_loaded_dict[str(label)], method="auto")
+    U1, p_unetpp = mannwhitneyu(spaderesnet_loaded_dict[str(label)], unetpp_loaded_dict[str(label)], method="auto")
+    print("P value for SPADESegResNet vs UNet: ",p_unet)
+    print("P value for SPADESegResNet vs UNet++: ",p_unetpp)
+    # boxplot = plt.boxplot([spaderesnet_loaded_dict[str(label)], unet_loaded_dict[str(label)], unetpp_loaded_dict[str(label)]], labels=labels, vert=True, patch_artist=True, medianprops=dict(color='black'))
+    # for box, color in zip(boxplot['boxes'], colors):
+    #     box.set_facecolor(color)
+    # plt.xticks(fontsize=18)
+    # plt.yticks(fontsize=18)
+    # # plt.show()
+    # plt.savefig(metric+'_'+tissue_id_dict[label]+'.png')
+    # plt.clf()
