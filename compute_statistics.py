f36eef8fd08a4aec0c39c72b8aedeb9f240ce0ad
Srijay Deshpande <deshpandesrijay@gmail.com>
2024-03-11 08:22:08 +0000

    Update compute_statistics.py

--- compute_statistics.py
+++ compute_statistics.py
@@ -9,11 +9,11 @@ import json
 import numpy as np
 from scipy.stats import mannwhitneyu
 
-spaderesnet_dice_path = r'F:\Datasets\BCSS_InstaDeep\splits\test\cropped\768\results_kaggle\spaderesnet\spaderesnet_auc.json'
-unet_dice_path = r'F:\Datasets\BCSS_InstaDeep\splits\test\cropped\768\results\unet\unet_auc.json'
-unetpp_dice_path = r'F:\Datasets\BCSS_InstaDeep\splits\test\cropped\768\results\unet++\unet++_auc.json'
+spaderesnet_dice_path = './output/spaderesnet_dice.json'
+unet_dice_path = './output/unet_dice.json'
+unetpp_dice_path = './output/unet++_dice.json'
 
-metric = 'AUC-ROC'
+metric = 'Dice score'
 
 tissue_id_dict = {1: 'Tumor', 2: 'Stroma', 3: 'Inflammatory', 4: 'Necrosis', 5: 'Other'}
 
@@ -37,11 +37,11 @@ for label in range(1, 6):
     U1, p_unetpp = mannwhitneyu(spaderesnet_loaded_dict[str(label)], unetpp_loaded_dict[str(label)], method="auto")
     print("P value for SPADESegResNet vs UNet: ",p_unet)
     print("P value for SPADESegResNet vs UNet++: ",p_unetpp)
-    # boxplot = plt.boxplot([spaderesnet_loaded_dict[str(label)], unet_loaded_dict[str(label)], unetpp_loaded_dict[str(label)]], labels=labels, vert=True, patch_artist=True, medianprops=dict(color='black'))
-    # for box, color in zip(boxplot['boxes'], colors):
-    #     box.set_facecolor(color)
-    # plt.xticks(fontsize=18)
-    # plt.yticks(fontsize=18)
-    # # plt.show()
-    # plt.savefig(metric+'_'+tissue_id_dict[label]+'.png')
-    # plt.clf()
+    boxplot = plt.boxplot([spaderesnet_loaded_dict[str(label)], unet_loaded_dict[str(label)], unetpp_loaded_dict[str(label)]], labels=labels, vert=True, patch_artist=True, medianprops=dict(color='black'))
+    for box, color in zip(boxplot['boxes'], colors):
+        box.set_facecolor(color)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    # plt.show()
+    plt.savefig(metric+'_'+tissue_id_dict[label]+'.png')
+    plt.clf()
